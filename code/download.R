e0730d5089a15f334e8dd354f14b2c40086ebad0
Kelly Sovacool <kellysovacool@gmail.com>
2020-02-04 10:35:45 -0500

    Support interactive oath (kindof)

--- code/download.R
+++ code/download.R
@@ -8,8 +8,18 @@ if (exists("snakemake")) {
 } else {
     filename_csv_raw <- here::here("data", "raw", "activities.csv")
 }
-filename_token <- here::here('.httr-oauth')
-stoken <- httr::config(token = readRDS(filename_token)[[1]])
+stoken_path <- '.httr-oauth'
+if (file.exists(stoken_path)) {
+    filename_token <- here::here(stoken_path)
+    stoken <- httr::config(token = readRDS(filename_token)[[1]])
+} else {
+    app_secret <-
+        readChar(client_secret_path, file.info(client_secret_path)$size)
+    stoken <-
+        httr::config(token = strava_oauth(appname, client_id, app_secret, 
+                                          app_scope = "activity:read_all",
+                                          cache=TRUE))
+}
 act_list <- get_activity_list(stoken)
 act_data_raw <- compile_activities(act_list)
 readr::write_csv(act_data_raw, filename_csv_raw)
