8ea92b91ef9db2db4b7ebd86a0b395344b60a70a
Kelly Sovacool <kellysovacool@gmail.com>
2020-02-25 09:42:27 -0500

    Fix weekday factor relevel

--- code/read_processed_data.R
+++ code/read_processed_data.R
@@ -5,7 +5,7 @@ read_data <- function(filename_csv) {
     data <- readr::read_csv(filename_csv) %>%
         mutate(type = fct_reorder(type, elapsed_time, .fun = sum, .desc=TRUE),
                wday = fct_relevel(fct_recode(as.factor(wday), 
-                                             "M"="2", "T"="3", "W"="4", "H"="5", "F"="6", "Sa"="7", "Su"="1"), 
+                                             "M"="1", "T"="2", "W"="3", "H"="4", "F"="5", "Sa"="6", "Su"="7"), 
                                   "M", "T", "W", "H", "F", "Sa", "Su") 
     )
 }
